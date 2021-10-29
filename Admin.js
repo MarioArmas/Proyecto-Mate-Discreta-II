ec817f98c84425ed82ec7e883061a8bcfe558ddb
danieelfcr <danielfcab2002@gmail.com>
2021-10-29 00:53:13 -0700

    Prototipos

---
+++ Admin.js
@@ -0,0 +1,29 @@
+void ingresarSitios()   //Este procedimiento permite el ingreso de un sitio turístico
+{
+    
+}
+
+void ingresarCarreteras()   //Este procedimiento permite el ingreso de carreteras
+{
+
+}
+
+void alertaCarreteras() //Este procedimiento alerta si una carretera está dañada
+{
+
+}
+
+void crearUsuarios()    //Este proedimiento permite al admin crear un usuario nuevo
+{
+    
+}
+
+void sitioDeshabilitado() //Este procedimiento permite alertar si un sitio turístico está deshabilitado
+{
+
+}
+
+void mostrarEstadisticas()   //Este procedimiento muestra las estadísticas
+{
+
+}
