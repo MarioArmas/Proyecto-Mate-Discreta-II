9dda6d5f13498e5416f10eddb7a76e8d5721d120
danieelfcr <danielfcab2002@gmail.com>
2021-10-30 01:52:00 -0700

    Prueba

--- Admin.js
+++ Admin.js
@@ -1,29 +1,95 @@
-void ingresarSitios()   //Este procedimiento permite el ingreso de un sitio turístico
-{
+function ingresarSitio() {
+    // validar que no hayan sitios repetidos
+    // añadir sitio a la base de datos con TODAS sus variables
+    var name = document.getElementById('nombre_sitio').value;
+    var latitud = document.getElementById('latitud').value;
+    var longitud = document.getElementById('longitud').value;
+    var disponible = true;
+    var carreteras = [];
+
+        //INGRESO A BASE DE DATOS
+    db.collection("Sitios").doc().set()({
+        name = document.getElementById('nombre_sitio').value,
+        latitud = document.getElementById('latitud').value,
+        longitud = document.getElementById('longitud').value,
+        disponible = true,
+
+    });
     
+    // verificar que no hayan campos vacios
+    if (name == "" || latitud == "" || longitud == "") {
+        return;
+    }
+
+    // codigo
 }
 
-void ingresarCarreteras()   //Este procedimiento permite el ingreso de carreteras
-{
+function ingresarCarretera() {
+    // añadir carreteras al vector del objeto "lugar" en la base de datos
+    // validar que no hayan carreteras repetidas
+    // validar que ambos vértices existan (lugares)
+    // las carreteras se van a añadir colocando el nombre del lugar destino en el objeto del lugar de inicio
+    var carretera_origen = document.getElementById('new_road_origen').value;
+    var carretera_destino = document.getElementById('new_road_destino').value;
 
+    // validar que no hayan campos vacios
+    if (carretera_origen == "" || carretera_destino == "") {
+        return;
+    }
+
+    // codigo
 }
 
-void alertaCarreteras() //Este procedimiento alerta si una carretera está dañada
-{
+function alertaCarretera() {
+    // añadir alerta a carreteras
+    // verificar que exista el lugar en la base de datos
+    // verificar que exista la carretera en la base de datos
+    // modificar el vector de carreteras del objeto "lugar" en la base de datos
+    // el modo en que lo vamos a hacer es añadiendo un "!" antes del nombre de la carretera
+    var carretera_origen = document.getElementById('damage_road_origen').value;
+    var carretera_destino = document.getElementById('damage_road_destino').value;
 
+    // validar que no hayan campos vacios
+    if (carretera_origen == "" || carretera_destino == "") {
+        return;
+    }
+
+    // codigo
 }
 
-void crearUsuarios()    //Este proedimiento permite al admin crear un usuario nuevo
-{
+function deshabilitarSitio() {
+    // verificar que el sitio exista
+    // cambiar la variable bool a false en la base de datos del sitio ingresado
+    var lugar = document.getElementById('site_disabled').value;
+
+    // validar que no hayan campos vacios
+    if (lugar == "") {
+        return;
+    }
+
+    // codigo
+}
+
+function habilitarSitio() {
+    // verificar que el sitio exista
+    // cambiar la variable bool a true en la base de datos del sitio ingresado
+    var lugar = document.getElementById('site_disabled').value;
     
+    // validar que no hayan campos vacios
+    if (lugar == "") {
+        return;
+    }
+
+    // codigo
 }
 
-void sitioDeshabilitado() //Este procedimiento permite alertar si un sitio turístico está deshabilitado
-{
+function showStats() {
+    // recoger todas las estadisticas que pide el doc de word y añadirlas al html
+    var etiqueta_html = document.getElementById('estadisticas');
+    var texto_estadisticas = "";
 
-}
+    // codigo
 
-void mostrarEstadisticas()   //Este procedimiento muestra las estadísticas
-{
-
+    // añadir datos al html
+    etiqueta_html.innerHTML = texto_estadisticas;
 }
