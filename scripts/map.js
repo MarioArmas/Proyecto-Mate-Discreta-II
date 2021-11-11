c993c2d600c5a1ac616310b6fe898f2cb38a5914
practicasfront <mario.r.armas@gmail.com>
2021-11-10 18:13:40 -0600

    Fix inputs to upgrade map

--- scripts/map.js
+++ scripts/map.js
@@ -1,7 +1,9 @@
 function showMap(){
-    var lat = parseFloat(document.getElementById("latitud").value)
-    var lng = parseFloat(document.getElementById("longitud").value)
+    const lat = parseFloat(document.getElementById("latitud").value)
+    const lng = parseFloat(document.getElementById("longitud").value)
     var coordenadas = {lat, lng};
+    console.log('test')
+
     var map = new google.maps.Map(document.getElementById('map'),{
         zoom: 10,
         center: coordenadas
@@ -11,10 +13,38 @@ function showMap(){
         map: map,
         draggable: true
     });
+    /* if (isNaN(lat) || isNaN(lng)) {
+    }
+    else {
+        const coords = {lat: 14.5947755, lng: -90.485321}
+        var map = new google.maps.Map(document.getElementById('map'),{
+            zoom: 10,
+            center: coords
+        });
+        var marker = new google.maps.Marker({
+            position: coords,
+            map: map,
+            draggable: true
+        });
+    } */
 
     // cambiar campos "latitud" y "longitud" por las coordenadas del mapa al soltar el marcador
     marker.addListener('dragend', function(event){
         document.getElementById("latitud").value = this.getPosition().lat();
         document.getElementById("longitud").value = this.getPosition().lng();
     })
+
 }
+
+function addShowMapToInput(inputID) {
+    const input = document.getElementById(inputID)
+    
+    input.addEventListener("input", (e) =>{
+        const value = parseFloat(input.value)
+        if (input.value != "" & !isNaN(value)) {
+            showMap()
+        }
+    })
+}
+addShowMapToInput('latitud')
+addShowMapToInput('longitud')
